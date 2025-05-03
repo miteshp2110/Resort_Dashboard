d767e91e3946418001e60e8d858251ace6a92bb8
miteshp2110 <paliwalmitesh2110@gmail.com>
2025-05-03 21:46:53 +0530

    Fixed login bug

--- context/auth-context.tsx
+++ context/auth-context.tsx
@@ -58,7 +58,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
   const login = async (username: string, password: string) => {
     try {
       setIsLoading(true)
-      const response = await fetch("http://localhost:3001/api/auth/login", {
+      const response = await fetch("https://playlist-backend.tech/resort/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
